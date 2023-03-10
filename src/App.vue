ee6536f33737d061eb9358634eb26be9cc84782d
jaime <759255348@qq.com>
2023-03-10 15:08:52 +0800

    new

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,6 @@ import { RouterLink, RouterView } from "vue-router";
     />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
